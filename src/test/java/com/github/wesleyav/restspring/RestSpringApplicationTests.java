f236efbad0f413fd0b4fd7692237fcbcc5684a1a
Wesley AV <13851280+wesleyav@users.noreply.github.com>
2022-08-01 16:20:16 -0300

    Adiciona estrutura do projeto

---
+++ src/test/java/com/github/wesleyav/restspring/RestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wesleyav.restspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
