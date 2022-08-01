0c37f2a7881f9a0f30426b07f893da02e634e992
Wesley AV <13851280+wesleyav@users.noreply.github.com>
2022-08-01 17:23:23 -0300

    Adiciona classe ProdutoService

---
+++ src/main/java/com/github/wesleyav/restspring/services/ProdutoService.java
@@ -0,0 +1,29 @@
+package com.github.wesleyav.restspring.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.wesleyav.restspring.entities.Produto;
+import com.github.wesleyav.restspring.repositories.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	public Produto save(Produto produto) {
+		return produtoRepository.save(produto);
+	}
+
+	public Produto findById(Long id) {
+		return produtoRepository.findById(id).orElse(null);
+	}
+
+	public List<Produto> findAll() {
+		return produtoRepository.findAll();
+	}
+
+}
