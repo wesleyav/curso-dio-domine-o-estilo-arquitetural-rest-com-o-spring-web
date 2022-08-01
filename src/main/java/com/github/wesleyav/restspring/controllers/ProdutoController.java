927dd7b44bd23f5ef4784f17c61cd81292aaa8a4
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-08-01 17:23:34 -0300

    Adiciona classe ProdutoController

---
+++ src/main/java/com/github/wesleyav/restspring/controllers/ProdutoController.java
@@ -0,0 +1,42 @@
+package com.github.wesleyav.restspring.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.wesleyav.restspring.entities.Produto;
+import com.github.wesleyav.restspring.services.ProdutoService;
+
+@RestController
+@RequestMapping(value = "/produto")
+public class ProdutoController {
+
+	@Autowired
+	private ProdutoService produtoService;
+
+	@PostMapping(value = "/save")
+	public ResponseEntity<Produto> salvaProduto(@RequestBody Produto produto) {
+		produto = produtoService.save(produto);
+		return ResponseEntity.ok().body(produto);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Produto> buscaProduto(@PathVariable Long id) {
+		Produto produto = produtoService.findById(id);
+		return ResponseEntity.ok().body(produto);
+	}
+
+	@GetMapping(value = "/busca-todos")
+	public ResponseEntity<List<Produto>> buscaTodosProdutos() {
+		List<Produto> produtos = produtoService.findAll();
+		return ResponseEntity.ok().body(produtos);
+	}
+
+}
