11e6d62b73a5f28667eb914e9a842ba0fadac14c
Wesley AV <13851280+wesleyav@users.noreply.github.com>
2022-08-02 09:41:21 -0300

    Adiciona configurações de JWT

--- src/main/java/com/github/wesleyav/restspring/config/SecurityConfig.java
+++ src/main/java/com/github/wesleyav/restspring/config/SecurityConfig.java
@@ -1,33 +1,64 @@
 package com.github.wesleyav.restspring.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.github.wesleyav.restspring.auth.services.UserService;
+import com.github.wesleyav.restspring.config.jwt.JwtAuthenticationEntryPoint;
+import com.github.wesleyav.restspring.config.jwt.JwtRequestFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf()
-		.disable()
-		.authorizeRequests()
-		.anyRequest()
-		.authenticated()
-			.and()
-			.httpBasic();
-	}
+	
+	@Autowired
+	public UserService userService;
 
 	@Autowired
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
+	
+	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		.withUser("kaique")
-		.password("{noop}123456")
-		.roles("USER");
+		auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
 	}
+	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+		httpSecurity.csrf().disable()
+				.authorizeRequests().antMatchers("/auth/signin").permitAll()
+				.antMatchers("/produto/**").hasAnyRole("ADMIN")
+				.antMatchers("/pedido/**").authenticated()
+				.anyRequest().authenticated().and()
+				.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+
+	
 
 }
