03c437288333120775f7b951ebac6ad08bbd837c
Wesley AV <13851280+wesleyav@users.noreply.github.com>
2022-08-02 10:44:02 -0300

    Adiciona configuração de Health Check para Actuator

---
+++ src/main/java/com/github/wesleyav/restspring/config/check/HealthCheck.java
@@ -0,0 +1,24 @@
+package com.github.wesleyav.restspring.config.check;
+
+import java.net.InetAddress;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+
+public class HealthCheck implements HealthIndicator {
+
+	@Override
+	public Health health() {
+
+		try {
+			InetAddress address = InetAddress.getByName("localhost");
+			if (address.isReachable(10000)) {
+				return Health.up().build();
+			}
+		} catch (Exception e) {
+			return Health.down().withDetail("Motivo: ", e.getMessage()).build();
+		}
+		return Health.down().withDetail("Motivo: ", "Motivo desconhecido").build();
+	}
+
+}
