669b06a0be9cda3fc1118bc49395d44b252e043a
Wesley AV <13851280+wesleyav@users.noreply.github.com>
2022-08-02 07:45:49 -0300

    Adiciona classe SecurityConfig - Spring Security

---
+++ src/main/java/com/github/wesleyav/restspring/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.github.wesleyav.restspring.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf()
+		.disable()
+		.authorizeRequests()
+		.anyRequest()
+		.authenticated()
+			.and()
+			.httpBasic();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("kaique")
+		.password("{noop}123456")
+		.roles("USER");
+	}
+
+}
