e4a9d52ba01437b3901abf4500d9f4c03b6620cd
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-08-02 09:39:17 -0300

    Adiciona classes de configuração do JWT

---
+++ src/main/java/com/github/wesleyav/restspring/config/jwt/JwtRequest.java
@@ -0,0 +1,24 @@
+package com.github.wesleyav.restspring.config.jwt;
+
+public class JwtRequest {
+
+	private String username;
+	private String senha;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
