d49d32ef74107e9e7ee3c57d5eea606931e07392
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-08-02 11:13:51 -0300

    Adiciona teste se valor negativo

--- src/test/java/com/github/wesleyav/restspring/ProdutoTest.java
+++ src/test/java/com/github/wesleyav/restspring/ProdutoTest.java
@@ -1,15 +1,29 @@
 package com.github.wesleyav.restspring;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import com.github.wesleyav.restspring.entities.Produto;
+import com.github.wesleyav.restspring.exceptions.ProductPriceException;
+import com.github.wesleyav.restspring.services.ProdutoService;
+
+@SpringBootTest
 public class ProdutoTest {
 
-	@Test
-	public void testeFalso() {
+	@Autowired
+	ProdutoService produtoService;
 
-		assertTrue(true);
+	@Test
+	public void verificaValorNegativoNoProduto() throws Exception {
+		Produto produto = new Produto();
+
+		produto.setNome("teste");
+		produto.setPreco(-10.0);
+
+		assertThrows(ProductPriceException.class, () -> produtoService.save(produto));
 	}
 
 }
