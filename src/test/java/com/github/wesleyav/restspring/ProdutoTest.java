0cc9764fb90c58bc058baf8ca9fa795f316bad4e
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-08-02 10:57:44 -0300

    Adiciona teste simples

---
+++ src/test/java/com/github/wesleyav/restspring/ProdutoTest.java
@@ -0,0 +1,15 @@
+package com.github.wesleyav.restspring;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ProdutoTest {
+
+	@Test
+	public void testeFalso() {
+
+		assertTrue(true);
+	}
+
+}
